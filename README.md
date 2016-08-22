05f3c4f29e86de2e10af6d01a7e63b040b04ba3c
Keiji Yahata <yahata@nowhere.co.jp>
2016-08-23 00:18:10 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# qiita2notebook
+
+Convert [Qiita](http://qiita.com/) articles to [Jupyter Notebook](https://jupyter.org/).
+
+## Requirements
+
+* Node 6.3.x
+
+## Install
+
+```
+npm install -g qiita2notebook
+```
+
+## Usage
+
+```
+qiita2notebook some_article_id > some_article_title.ipynb
+```
+
+## Example
+
+```
+qiita2notebook b328fe0116d248127a36 > 2015年センター試験数学IAを全てプログラム(Python)で解く.ipynb
+```
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,102 @@
+#! /usr/bin/env node
+const fetch = require("node-fetch");
+const url = "http://qiita.com/api/v2/items/";
+const item_id = process.argv[2];
+const template = {
+	ipynb: {
+		"cells": null,
+		"metadata": {
+			"kernelspec": {
+				"display_name": "Python 2",
+				"language": "python",
+				"name": "python2"
+			},
+			"language_info": {
+				"codemirror_mode": {
+					"name": "ipython",
+					"version": 2
+				},
+				"file_extension": ".py",
+				"mimetype": "text/x-python",
+				"name": "python",
+				"nbconvert_exporter": "python",
+				"pygments_lexer": "ipython2",
+				"version": "2.7.10"
+			}
+		},
+		"nbformat": 4,
+		"nbformat_minor": 1
+	},
+	markdownCell: {
+		"cell_type": "markdown",
+		"metadata": {},
+		"source": null
+	},
+	codeCell: {
+		"cell_type": "code",
+		"execution_count": null,
+		"metadata": {
+			"collapsed": false
+		},
+		"outputs": [],
+		"source": null
+	}
+};
+
+fetch(url + item_id)
+	.then(res => {
+		return res.json();
+	})
+	.then(json => {
+		const markdown = json.body;
+		const ipynb = md2ipynb(markdown);
+
+		console.log(JSON.stringify(ipynb, null, " "));
+	})
+	.catch(err => console.error(err));
+
+/**
+ *	@param {String} markdown
+ *	@return {Object}
+ */
+function md2ipynb(markdown) {
+	const lines = markdown.split("\n");
+	const ipynb = Object.assign({}, template.ipynb);
+
+	let isCodeBlock = false;
+	let cell = Object.assign({}, template.markdownCell);
+
+	ipynb.cells = [];
+	cell.source = [];
+
+	lines.forEach(line => {
+		// starting line of code block
+		if (line.match(/^```py/)) {
+			ipynb.cells.push(cell);
+			cell = Object.assign({}, template.codeCell);
+			cell.source = [];
+			isCodeBlock = true;
+
+		// ending line of code block
+		} else if (isCodeBlock && line.match(/^```$/)) {
+			ipynb.cells.push(cell);
+			cell = Object.assign({}, template.markdownCell);
+			cell.source = [];
+			isCodeBlock = false;
+
+		// inside code block
+		} else if (isCodeBlock) {
+			cell.source.push(line + "\n");
+
+		// normal block
+		} else {
+			cell.source.push(line + "\n");
+		}
+	});
+
+	ipynb.cells.push(cell);
+
+	return ipynb;
+}
+
+exports.md2ipynb = md2ipynb;
